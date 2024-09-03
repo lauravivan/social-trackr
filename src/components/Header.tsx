00aa1182d0b967427eecd5bb3b52fefd7971e426
lauravivan <lauvivang@gmail.com>
2024-09-03 11:50:13 -0300

    Add header content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,31 @@
+import { Github, Linkedin, Twitter } from "./icons";
+import { GITHUB_LINK, LINKEDIN_LINK, TWITTER_LINK } from "../util";
+
 interface HeaderType {
   children: string;
 }
 
 export function Header({ children }: HeaderType) {
   return (
-    <header>
-      <h1>{children}</h1>
+    <header className="header">
+      <h1 className="header__title">{children}</h1>
+      <div className="header__social-media">
+        <div className="header__social-media--item">
+          <a target="_blank" href={GITHUB_LINK}>
+          <Github />
+          </a>
+        </div>
+        <div className="header__social-media--item">
+          <a target="_blank" href={TWITTER_LINK}>
+          <Twitter />
+          </a>
+        </div>
+        <div className="header__social-media--item">
+          <a target="_blank" href={LINKEDIN_LINK}>
+          <Linkedin />
+          </a>
+        </div>
+      </div>
     </header>
   );
 }
